6851a300838075bc1ab5a7e19998e540d9a77f20
yuval <yuvalell@post.bgu.ac.il>
2025-11-12 12:36:17 +0200

    First commit - added task files

---
+++ hw1.py
@@ -0,0 +1,18 @@
+import pyodbc
+
+
+class DatabaseManager:
+    def __init__(self, driver: str, server: str, username: str, password: str, database: str):
+        pass  # TODO
+
+    def file_to_database(self, path: str) -> None:
+        pass  # TODO
+
+    def add_summary_items(self) -> None:
+        pass  # TODO
+
+    def get_best_films(self) -> dict[int, str]:
+        pass  # TODO
+
+    def get_n_best_years(self, n: int) -> list[int]:
+        pass  # TODO
